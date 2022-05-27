920e778c341380bfe99190a0b34d7bbd2059b842
Warren Fletcher <warren@fletch.band>
2022-05-27 22:31:18 +0200

    Fixed Layout, Added JQuery, Getting ready for development

--- node-ex-website/express/js/scripts.js
+++ node-ex-website/express/js/scripts.js
@@ -1,4 +1,4 @@
-function stuff(){
+function stuff() {
     axios({
         method: 'get',
         url: 'http://localhost:3000/get_snapshot',
@@ -6,32 +6,45 @@ function stuff(){
             'Cache-Control': 'no-cache',
             'Pragma': 'no-cache',
             'Expires': '0',
-          },
-      })
+        },
+    })
         .then(function (response) {
             console.log(response)
-        data = response["data"]
-        var nodes = new vis.DataSet(
-           data["nodes"]
-        );
-    
-        // create an array with edges
-        var edges = new vis.DataSet(
-            data["edges"]
-        );
-    
-        // create a network
-        var container = document.getElementById('vis_container');
-    
-        // provide the data in the vis format
-        var data = {
-            nodes: nodes,
-            edges: edges
-        };
-        var options = {};
-    
-        // initialize your network!
-        var network = new vis.Network(container, data, options);
+            data = response["data"]
+            var nodes = new vis.DataSet(
+                data["nodes"]
+            );
+
+            // create an array with edges
+            var edges = new vis.DataSet(
+                data["edges"]
+            );
+
+            // create a network
+            var container = document.getElementById('vis_container');
+
+            // provide the data in the vis format
+            var data = {
+                nodes: nodes,
+                edges: edges
+            };
+            var options = {
+                nodes: {
+                    shape: 'dot'
+                },
+                edges: {
+                    smooth: false
+                },
+                physics: false,
+                interaction: {
+                    dragNodes: false,// do not allow dragging nodes
+                    zoomView: false, // do not allow zooming
+                    dragView: false  // do not allow dragging
+                }
+            };// initialize your network!
+            var network = new vis.Network(container, data, options);
         });
-        }
-        setInterval(function(){stuff()},1000);
+}
+$( document ).ready(function() {
+    setInterval(function () { stuff() }, 1000);
+});
--- node-ex-website/server.js
+++ node-ex-website/server.js
@@ -7,18 +7,18 @@ const fs = require('fs');
 app.use(express.json());
 app.use(express.static("express"));
 // default URL for website
-app.get('/home', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-app.get("/get_snapshot", function(req,res) {
+app.get('/home', function (req, res) {
+  res.sendFile(path.join(__dirname + '/express/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.get("/get_snapshot", function (req, res) {
   // TODO: Move this to another router
   var files = fs.readdirSync('./snapshots/');
-    console.log(files);
-    file_name = files[0]
-    const jsonString = fs.readFileSync("./snapshots/" + file_name);
-    const data = JSON.parse(jsonString);
-    res.json(data);
+  console.log(files);
+  file_name = files[0]
+  const jsonString = fs.readFileSync("./snapshots/" + file_name);
+  const data = JSON.parse(jsonString);
+  res.json(data);
 });
 const server = http.createServer(app);
 const port = 3000;
